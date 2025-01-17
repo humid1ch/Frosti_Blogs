5e51a279a06da862469c879ac93f24c91f6f4f3d
humid1ch <dxyt2002@163.com>
2025-01-18 01:23:07 +0800

    modify toc and back2top positions

--- src/consts.ts
+++ src/consts.ts
@@ -2,16 +2,16 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // Site title and description
-export const SITE_LANG = "en";
-export const SITE_TAB = "Frosti";
-export const SITE_TITLE = "Frosti 🧊";
-export const SITE_DESCRIPTION = "A blog template";
+export const SITE_LANG = "zh";
+export const SITE_TAB = "humid1ch";
+export const SITE_TITLE = "humid1ch's blogs";
+export const SITE_DESCRIPTION = "A park";
 export const DATE_FORMAT = "ddd MMM DD YYYY";
 
 // User profile information
-export const USER_NAME = "EveSunMaple";
-export const USER_SITE = "https://frosti.saroprock.com"; // At the same time, this is also the site retrieved by the i18n configuration.
-export const USER_AVATAR = "/profile.webp";
+export const USER_NAME = "哈米d1ch";
+export const USER_SITE = "https://www.humid1ch.cn"; // At the same time, this is also the site retrieved by the i18n configuration.
+export const USER_AVATAR = "/profile.png";
 
 // Server and transition settings
 export const SERVER_URL = "https://demo.saroprock.com";
@@ -28,55 +28,55 @@ export const CODE_THEME = {
 
 // Menu items for navigation
 export const menuItems = [
-  { id: "home", text: "Home", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
-  { id: "about", text: "About", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
+  { id: "home", text: "主页", href: "/", svg: "material-symbols:home-outline-rounded", target: "_self" }, // Home page
+  { id: "about", text: "关于", href: "/about", svg: "material-symbols:info-outline-rounded", target: "_self" }, // About page
   {
     id: "blog",
-    text: "Blogs",
+    text: "博客",
     href: "/blog",
     svg: "material-symbols:book-2-outline-rounded",
     target: "_self",
     subItems: [
       {
         id: "all",
-        text: "All blogs",
+        text: "所有博客",
         href: "/blog",
         svg: "material-symbols:ink-pen-outline-rounded",
         target: "_self",
       }, // All blog
       {
         id: "tech",
-        text: "Tech blogs",
+        text: "技术",
         href: "/blog/categories/tech",
         svg: "material-symbols:deployed-code-outline",
         target: "_self",
       }, // Technology category
       {
         id: "life",
-        text: "Life blogs",
+        text: "随笔",
         href: "/blog/categories/life",
         svg: "material-symbols:earthquake-rounded",
         target: "_self",
       }, // Life category
     ],
   }, // Blog page with sub-items
-  {
-    id: "project",
-    text: "Project",
-    href: "/project",
-    svg: "material-symbols:code-blocks-outline",
-    target: "_self",
-  }, // Projects page
+  // {
+  //   id: "project",
+  //   text: "Project",
+  //   href: "/project",
+  //   svg: "material-symbols:code-blocks-outline",
+  //   target: "_self",
+  // }, // Projects page
   {
     id: "friend",
-    text: "Friend",
+    text: "友链",
     href: "/friend",
     svg: "material-symbols:supervisor-account-outline-rounded",
     target: "_self",
   }, // Friends page
   {
     id: "contact",
-    text: "Contact",
+    text: "联络",
     href: "mailto:contact.evesunmaple@outlook.com", // Contact email
     target: "_blank", // Open in a new tab
     svg: "material-symbols:attach-email-outline-rounded",
@@ -85,24 +85,24 @@ export const menuItems = [
 
 // Social media and contact icons
 export const socialIcons = [
+  // {
+  //   href: "https://afdian.net/a/saroprock",
+  //   ariaLabel: "Support my work",
+  //   title: "Support my work",
+  //   svg: "ri:cup-line",
+  // },
   {
-    href: "https://afdian.net/a/saroprock",
-    ariaLabel: "Support my work",
-    title: "Support my work",
-    svg: "ri:cup-line",
-  },
-  {
-    href: "https://github.com/EveSunMaple",
+    href: "https://github.com/humid1ch",
     ariaLabel: "Github",
     title: "Github",
     svg: "ri:github-line",
   },
-  {
-    href: "https://space.bilibili.com/438392347",
-    ariaLabel: "BiliBili",
-    title: "BiliBili",
-    svg: "ri:bilibili-line",
-  },
+  // {
+  //   href: "https://space.bilibili.com/438392347",
+  //   ariaLabel: "BiliBili",
+  //   title: "BiliBili",
+  //   svg: "ri:bilibili-line",
+  // },
   {
     href: "/rss.xml",
     ariaLabel: "RSS Feed",
